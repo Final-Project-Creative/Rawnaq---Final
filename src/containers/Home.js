b05f65ce700cbbcf7be941c63e045d5e8ffb956b
Abed Alamleh <a.j.alamleh"gmail.com>
2020-12-23 20:39:14 +0200

    Category Steeling

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -37,51 +37,46 @@ const HomepageLayout = () => {
           </div>
         </div>
       </Slide>
+
       <div className="container">
         <div className="row" style={{ textAlign: 'center' }}>
           <div className="col-lg-1 col-xs-1">
-
           </div>
           <div className="col-lg-5 col-xs-10">
-            <h1 className='title'>WOMEN</h1>
             <Link className='background-image' to="/womenproducts">
+              <h1 className='title'>WOMEN</h1>
               <img className='background-image' width="100%" src='https://image.made-in-china.com/2f0j00oOStnyLzMgcF/Modern-Fashion-Elegant-Women-Clothes.jpg'></img>
             </Link>
           </div>
           <div className="col-xs-1">
-
           </div>
           <div className="col-xs-1">
-
           </div>
           <div className="col-lg-5 col-xs-10">
-            <h1 className='title'>MEN</h1>
             <Link className='background-image' to="/menproducts">
+              <h1 className='title'>MEN</h1>
               <img className='background-image' width="100%" src='https://cf.shopee.ph/file/1a4c08a2503d9ada5307c116673328ab'></img>
             </Link>
           </div>
           <div className="col-lg-1">
-
           </div>
         </div>
         <div className="row" style={{ textAlign: 'center' }}>
           <div className="col-lg-1">
-
           </div>
           <div className="col-lg-5">
-            <h1 className='title'>CHILDREN</h1>
             <Link className='background-image' to="/childproducts">
+              <h1 className='title'>CHILDREN</h1>
               <img className='background-image' width="100%" src='https://previews.123rf.com/images/olgagi/olgagi1904/olgagi190400207/121197640-collage-set-of-children-clothes-denim-jacket-and-pants-shoes-and-rain-jacket-for-child-boy-isolated-.jpg'></img>
             </Link>
           </div>
           <div className="col-lg-5">
-            <h1 className='title'>accessory</h1>
             <Link className='background-image' to="/accproducts">
+              <h1 className='title'>ACCESSORIES</h1>
               <img className='background-image' width="100%" src='https://image.shutterstock.com/image-photo/fashion-women-stylish-accessories-outfit-260nw-1532053424.jpg'></img>
             </Link>
           </div>
           <div className="col-lg-1">
-
           </div>
         </div>
       </div>
