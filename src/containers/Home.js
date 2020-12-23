e089ddd6d4f47fa79e010e05870638575b6c22d1
Abed Alamleh <a.j.alamleh"gmail.com>
2020-12-23 19:44:40 +0200

    Category Steeling

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -85,56 +85,6 @@ const HomepageLayout = () => {
           </div>
         </div>
       </div>
-
-
-      {/* <div className='homepage'>
-        <div className='directory-menu'>
-          <div className='menu-item'>
-            <div className='content'>
-              <h1 className='title'>WOMEN</h1>
-              <Link className='background-image' to="/womenproducts">
-                <img className='background-image' src='https://image.made-in-china.com/2f0j00oOStnyLzMgcF/Modern-Fashion-Elegant-Women-Clothes.jpg'></img>
-              </Link>
-              <span className='subtitle'>Shop Now</span>
-
-            </div>
-          </div>
-
-          <div className='menu-item'>
-            <div className='content'>
-              <h1 className='title'>MEN</h1>
-              <Link className='background-image' to="/menproducts">
-                <img className='background-image' src='https://cf.shopee.ph/file/1a4c08a2503d9ada5307c116673328ab'></img>
-              </Link>
-              <span className='subtitle'>Shop Now</span>
-
-            </div>
-          </div>
-
-          <div className='menu-item'>
-            <div className='content'>
-              <h1 className='title'>CHILDREN</h1>
-              <Link className='background-image' to="/childproducts">
-                <img className='background-image' src='https://previews.123rf.com/images/olgagi/olgagi1904/olgagi190400207/121197640-collage-set-of-children-clothes-denim-jacket-and-pants-shoes-and-rain-jacket-for-child-boy-isolated-.jpg'></img>
-              </Link>
-              <span className='subtitle'>Shop Now</span>
-
-            </div>
-          </div>
-
-          <div className='menu-item'>
-            <div className='content'>
-              <h1 className='title'>accessory</h1>
-              <Link className='background-image' to="/accproducts">
-                <img className='background-image' src='https://image.shutterstock.com/image-photo/fashion-women-stylish-accessories-outfit-260nw-1532053424.jpg'></img>
-              </Link>
-              <span className='subtitle'>Shop Now</span>
-
-            </div>
-          </div>
-
-        </div>
-      </div> */}
     </div>
   )
 }
